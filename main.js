e9c3537f89cfc8e9e6ebf30ce90ecf4634597a12
isacord <111879423+isacord@users.noreply.github.com>
2024-04-01 10:59:49 -0300

    Update main.js

--- main.js
+++ main.js
@@ -13,3 +13,20 @@ for(let i=0;i<botoes.length;i++){
         textos[i].classlist.add("ativo");
     }
 }
+const contadores= document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date (2023-10-05T00:00:000);
+
+contadores[0].textContent = calculadoraTempo(tempoObjetivo10);
+
+function calculadoraTempo(tempoObjetivo1){
+    let tempoAtual = newDate();
+    let tempoFinal = newDate();
+    let segundos = Math.floor(tempoFinal/1000);
+    let horas = Math.floor(minutos/60);
+    let dias = Math;floor(horas/24);
+
+    segundos % = 60;
+    minutos % = 60;
+    horas % = 24;
+
+}
